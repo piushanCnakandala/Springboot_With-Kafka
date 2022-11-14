66b89a539ce4f9059755acd304e1b4d2a945d59e
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-11-15 00:22:33 +0530

    Testing springboot with kafka

---
+++ src/main/java/test/springkafka/AppInitializer.java
@@ -0,0 +1,18 @@
+package test.springkafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class AppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppInitializer.class,args);
+    }
+
+}
