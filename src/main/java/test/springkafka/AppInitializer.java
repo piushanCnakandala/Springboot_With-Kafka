32e84aeb487a725e29c4cc07a64834b88fda89e0
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-11-15 22:51:50 +0530

    update

--- src/main/java/test/springkafka/AppInitializer.java
+++ src/main/java/test/springkafka/AppInitializer.java
@@ -1,18 +1,26 @@
 package test.springkafka;
 
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.boot.ApplicationRunner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
+
 
 @SpringBootApplication
 public class AppInitializer {
     public static void main(String[] args) {
         SpringApplication.run(AppInitializer.class,args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+        return args -> {
+            for (int i = 0; i < 100; i++) {
+                kafkaTemplate.send("testing", "Testinggggg "+i);
+            }
+        };
     }
 
 }
